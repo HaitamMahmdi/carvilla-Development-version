d7a0fab018a056a505c27dd8790f13710ada9285
unknown <haitammaharani@gmail.com>
2026-05-17 16:29:02 +0200

    delete vuex and add pinia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import { createPinia } from "pinia";
 import "normalize.css";
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -9,4 +9,5 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./sass/_glople-file.scss";
 //import "bootstrap/dist/js/bootstrap.bundle";
 //import "./sass/_custom.scss";
-createApp(App).use(store).use(router).mount("#app");
+const pinia = createPinia();
+createApp(App).use(pinia).use(router).mount("#app");
